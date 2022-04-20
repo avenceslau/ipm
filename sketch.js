119f8be9a72f913362edeeff64fdf6b2b02d7a67
1-0-GO <martinsfrancisco01@gmail.com>
2022-04-20 22:59:31 +0100

    Voltar atrás a apenas snapping

--- sketch.js
+++ sketch.js
@@ -15,11 +15,6 @@ let TARGET_SIZE;
 let TARGET_PADDING, MARGIN, LEFT_PADDING, TOP_PADDING;
 let continue_button;
 let inputArea = { x: 0, y: 0, h: 0, w: 0 } // Position and size of the user input area
-let bcgColor;
-let prev_click_x;
-let prev_click_y;
-//let sound;
-//let sound_play;
 
 // Metrics
 let testStartTime, testEndTime; // time between the start and end of one attempt (54 trials)
@@ -48,11 +43,6 @@ class Target {
 function setup() {
     createCanvas(700, 500); // window size in px before we go into fullScreen()
     frameRate(60); // frame rate (DO NOT CHANGE!)
-    let target = getTargetBounds(0);
-    prev_click_x = target.x;
-    prev_click_y = target.y;
-
-    bcgColor = color(0, 0, 0);
 
     randomizeTrials(); // randomize the trial order at the start of execution
 
@@ -62,86 +52,39 @@ function setup() {
 
 
 function Fitts(x1, y1, x2, y2, radius) {
-    let a = x1 - x2;
-    let b = y1 - y2;
-    let dist = Math.sqrt(a * a + b * b);
-    return Math.log2(1 + (dist / radius));
+    a = x1 - x2;
+    b = y1 - y2;
+    dist = Math.sqrt(a * a + b * b);
+    return Math.log2((dist / radius) + 1);
 }
 
-//Plot line to next target and write text 2x when applicable
-function draw_aux() {
-    let prev_target = getTargetBounds(trials[current_trial - 1]);
-    let curr_target = getTargetBounds(trials[current_trial]);
-    let next_target = getTargetBounds(trials[current_trial + 1]);
-    let input_prev_target = new Target(map(prev_target.x, 0, width, inputArea.x, inputArea.x + inputArea.w),
-        map(prev_target.y, 0, height, inputArea.y, inputArea.y + inputArea.h),
-        map(prev_target.w, 0, 0.5 * PPCM, 0, 15));
-    let input_curr_target = new Target(map(curr_target.x, 0, width, inputArea.x, inputArea.x + inputArea.w),
-        map(curr_target.y, 0, height, inputArea.y, inputArea.y + inputArea.h),
-        map(curr_target.w, 0, 0.5 * PPCM, 0, 15));
-    let input_next_target = new Target(map(next_target.x, 0, width, inputArea.x, inputArea.x + inputArea.w),
-        map(next_target.y, 0, height, inputArea.y, inputArea.y + inputArea.h),
-        map(next_target.w, 0, 0.5 * PPCM, 0, 15));
-
-
+//Plot line to next target
+function draw_lines() {
     stroke(color(255, 0, 0));
-    line(curr_target.x, curr_target.y, next_target.x, next_target.y);
-    strokeWeight(3);
-    line(input_curr_target.x, input_curr_target.y, input_next_target.x, input_next_target.y);
-    stroke(color(0, 255, 0));
-    line(input_prev_target.x, input_prev_target.y, input_curr_target.x, input_curr_target.y);
-
-    if (dist(curr_target.x, curr_target.y, next_target.x, next_target.y) === 0) {
-        noStroke();
-        textFont("Arial", 32);
-        fill(color(0, 0, 255));
-        textAlign(CENTER);
-        text("2x", curr_target.x, curr_target.y + 16);
-        textFont("Arial", 24);
-        text("2x", input_curr_target.x, input_curr_target.y + 8);
-    }
+    line(getTargetBounds(trials[current_trial]).x, getTargetBounds(trials[current_trial]).y, getTargetBounds(trials[current_trial + 1]).x, getTargetBounds(trials[current_trial + 1]).y);
 }
 
 // Runs every frame and redraws the screen
 function draw() {
     if (draw_targets) {
         // The user is interacting with the 6x3 target grid
-        background(bcgColor); // sets background to black
+        //FIXME background
+        background(color(0, 0, 0)); // sets background to black
 
         // Print trial count at the top left-corner of the canvas
         fill(color(255, 255, 255));
         textAlign(LEFT);
-        textFont("Arial", 20);
-        text("Trial " + (current_trial + 1) + " of " + trials.length, 50, 25);
-        text("MousePos: " + mouseX + ", " + mouseY, 50, 50);
-
-        textAlign(RIGHT);
-        textFont("Arial", 32);
-        text("Espera! O tempo ainda não está a contar. Lê isto primeiro:", width, inputArea.y - 1.15 * inputArea.h);
-        textFont("Arial", 24);
-        text("Acerta no alvo a verde, quando o alvo muda de cor podes carregar que contará como hit.", width, inputArea.y - 0.90 * inputArea.h);
-        text("Quando aparecer o texto 2x dentro do alvo faz duplo clique - o próximo alvo é o mesmo.", width, inputArea.y - 0.75 * inputArea.h);
-        text("A área de input é esta, podes esquecer o que está à esquerda e apenas tentar acertar", width, inputArea.y - 0.60 * inputArea.h);
-        text("nos alvos olhando para aqui.", width, inputArea.y - 0.45 * inputArea.h);
-        beginShape();
-        vertex(inputArea.x + inputArea.w, inputArea.y);
-        vertex(inputArea.x + (1 - 1 / 18) * inputArea.w, (1 - 1 / 10) * inputArea.y);
-        vertex(inputArea.x + (1 - 1 / 36) * inputArea.w, (1 - 1 / 10) * inputArea.y);
-        vertex(inputArea.x + (1 - 1 / 36) * inputArea.w, (1 - 1 / 4) * inputArea.y);
-        vertex(inputArea.x + (1 + 1 / 36) * inputArea.w, (1 - 1 / 4) * inputArea.y);
-        vertex(inputArea.x + (1 + 1 / 36) * inputArea.w, (1 - 1 / 10) * inputArea.y);
-        vertex(inputArea.x + (1 + 1 / 18) * inputArea.w, (1 - 1 / 10) * inputArea.y);
-        endShape();
+        text("Trial " + (current_trial + 1) + " of " + trials.length, 50, 20);
 
         // Draw all 18 targets
         for (var i = 0; i < 18; i++) drawTarget(i);
-        draw_aux();
+        draw_lines();
         // Draw the user input area
-        drawInputArea();
+        drawInputArea()
 
         // Draw the virtual cursor
-        let x = map(mouseX, inputArea.x, inputArea.x + inputArea.w, 0, width);
-        let y = map(mouseY, inputArea.y, inputArea.y + inputArea.h, 0, height);
+        let x = map(mouseX, inputArea.x, inputArea.x + inputArea.w, 0, width)
+        let y = map(mouseY, inputArea.y, inputArea.y + inputArea.h, 0, height)
         for (var i = 0; i < 18; i++) {
             if (getTargetBounds(i).x - 1.5 * PPCM < x && x < getTargetBounds(i).x + 1.5 * PPCM && getTargetBounds(i).y - 1.5 * PPCM < y && y < getTargetBounds(i).y + 1.5 * PPCM) {
                 x = getTargetBounds(i).x;
@@ -176,22 +119,7 @@ function printAndSavePerformance() {
     text("Average time per target: " + time_per_target + "s", width / 2, 180);
     text("Average time for each target (+ penalty): " + target_w_penalty + "s", width / 2, 220);
 
-
     // Print Fitts IDS (one per target, -1 if failed selection, optional)
-    for (var i = 0, col_y = 270; i < 27; col_y += 27, i++) {
-        if (i == 0)
-            text("Target " + (i + 1) + ": - - -", width * 1 / 3, col_y);
-        else if (fitts_IDs[i] == -1)
-            text("Target " + (i + 1) + ": MISSED", width * 1 / 3, col_y);
-        else
-            text("Target " + (i + 1) + ": " + fitts_IDs[i], width * 1 / 3, col_y);
-    }
-    for (var i = 27, col_y = 270; i < 54; col_y += 27, i++) {
-        if (fitts_IDs[i] == -1)
-            text("Target " + (i + 1) + ": MISSED", width * 2 / 3, col_y);
-        else
-            text("Target " + (i + 1) + ": " + fitts_IDs[i], width * 2 / 3, col_y);
-    }
     // 
 
     // Saves results (DO NOT CHANGE!)
@@ -231,6 +159,8 @@ function mousePressed() {
         // Get the location and size of the target the user should be trying to select
         let target = getTargetBounds(trials[current_trial]);
 
+
+
         // Check to see if the virtual cursor is inside the target bounds,
         // increasing either the 'hits' or 'misses' counters
 
@@ -244,16 +174,10 @@ function mousePressed() {
                 }
             }
 
-            if (dist(target.x, target.y, virtual_x, virtual_y) < target.w / 2) {
-                hits++;
-                let temp = Fitts(prev_click_x, prev_click_y, target.x, target.y, target.w);
-                fitts_IDs.push(temp.toFixed(3));
-            } else {
-                misses++;
-                fitts_IDs.push(-1);
-            }
-            prev_click_x = virtual_x;
-            prev_click_y = virtual_y;
+
+            if (dist(target.x, target.y, virtual_x, virtual_y) < target.w / 2) hits++;
+            else misses++;
+
             current_trial++; // Move on to the next trial/target
         }
 
@@ -278,36 +202,20 @@ function mousePressed() {
 function drawTarget(i) {
     // Get the location and size for target (i)
     let target = getTargetBounds(i);
-    let input_target = new Target(map(target.x, 0, width, inputArea.x, inputArea.x + inputArea.w),
-        map(target.y, 0, height, inputArea.y, inputArea.y + inputArea.h),
-        map(target.w, 0, 0.5 * PPCM, 0, 15));
+
 
     // Check whether this target is the target the user should be trying to select
     if (trials[current_trial] === i) {
         //FIXME target
         // Highlights the target the user should be trying to select
-        // with green filling 
+        // with a white border
         fill(color(0, 255, 0));
         noStroke();
+        //stroke(color(255, 255, 0));
+        //strokeWeight(6);
         circle(target.x, target.y, target.w);
-        square(input_target.x - input_target.w / 2, input_target.y - input_target.w / 2, input_target.w, 8);
-        if (insideInputArea(mouseX, mouseY)) {
-            let virtual_x = map(mouseX, inputArea.x, inputArea.x + inputArea.w, 0, width)
-            let virtual_y = map(mouseY, inputArea.y, inputArea.y + inputArea.h, 0, height)
-            for (var i = 0; i < 18; i++) {
-                if (getTargetBounds(i).x - 1.5 * PPCM < virtual_x && virtual_x < getTargetBounds(i).x + 1.5 * PPCM && getTargetBounds(i).y - 1.5 * PPCM < virtual_y && virtual_y < getTargetBounds(i).y + 1.5 * PPCM) {
-                    virtual_x = getTargetBounds(i).x;
-                    virtual_y = getTargetBounds(i).y;
-                }
-            }
 
-            if (dist(target.x, target.y, virtual_x, virtual_y) < target.w / 2) {
-                fill(0, 255, 255);
-                let width = input_target.w + 12;
-                square(input_target.x - width / 2, input_target.y - width / 2, width, 8);
-                return;
-            }
-        }
+
 
         // Remember you are allowed to access targets (i-1) and (i+1)
         // if this is the target the user should be trying to select
@@ -317,21 +225,18 @@ function drawTarget(i) {
     // Does not draw a border if this is not the target the user
     // should be trying to select
     else {
+        // FIXME not the target
         noStroke();
         // Draws the target
         fill(color(0, 4, 74));
         circle(target.x, target.y, target.w);
-        fill(color(2, 4, 16));
-        square(input_target.x - input_target.w / 2, input_target.y - input_target.w / 2, input_target.w, 8);
     }
 
     if (trials[current_trial + 1] === i) {
+
         stroke(color(255, 0, 0));
         strokeWeight(6);
-        fill(color(0, 4, 74));
         circle(target.x, target.y, target.w);
-        fill(color(2, 4, 16));
-        square(input_target.x - input_target.w / 2, input_target.y - input_target.w / 2, input_target.w, 8);
     }
 }
 
